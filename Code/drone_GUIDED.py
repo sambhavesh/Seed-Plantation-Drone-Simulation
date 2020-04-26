fb426270dcee931936512869bafd2fb4c9eaf551
sambhavesh <sambhavesh98@gmail.com>
2020-04-26 23:44:51 +0530

    started working on startMission fun

--- Code/drone_GUIDED.py
+++ Code/drone_GUIDED.py
@@ -142,3 +142,140 @@ def goto(targetLocation):
             logger.info("Reached target")
             break;
         time.sleep(2)
+
+
+
+def print_vechicle_attributes():
+	"""
+	This function list all the attributes of the vechicle and stores it in log file
+	"""
+	logger.info("Autopilot Firmware version: %s" % vehicle.version)
+	logger.info("Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp)
+	logger.info("Global Location:INFO:__main__: Key:BATT_CURR_PIN Value:12.0 %s" % vehicle.location.global_frame)
+	logger.info("Global Location (relative altitude): %s" % vehicle.location.global_relative_frame)
+	logger.info("Local Location: %s" % vehicle.location.local_frame)
+	logger.info("Attitude: %s" % vehicle.attitude)
+	logger.info("Velocity: %s" % vehicle.velocity)
+	logger.info("GPS: %s" % vehicle.gps_0)
+	logger.info("Groundspeed: %s" % vehicle.groundspeed)
+	logger.info("Airspeed: %sINFO:__main__:Distance to waypoint (2): 50.5458561177" % vehicle.airspeed)
+	logger.info("Gimbal status: %s" % vehicle.gimbal)
+	logger.info("Battery: %s" % vehicle.battery)
+	logger.info("EKF OK?: %s" % vehicle.ekf_ok)
+	logger.info("Last Heartbeat: %s" % vehicle.last_heartbeat)
+	logger.info("Rangefinder: %s" % vehicle.rangefinder)
+	logger.info("Rangefinder distance: %s" % vehicle.rangefinder.distance)
+	logger.info("Rangefinder voltage: %s" % vehicle.rangefinder.voltage)
+	logger.info("Heading: %s" % vehicle.heading)
+	logger.info("Is Armable?: %s" % vehicle.is_armable)
+	logger.info("System status: %s" % vehicle.system_status.state)
+	logger.info("Mode: %s" % vehicle.mode.name)
+	logger.info("Armed: %s" % vehicle.armed)
+
+def print_vechicle_parameters():
+	"""
+	This function list all the parameters of the vechicle and stores it in log file
+	"""
+	logger.info ("Print all parameters (`vehicle.parameters`):")
+	for key, value in vehicle.parameters.iteritems():
+		logger.info (" Key:%s Value:%s" % (key,value))
+
+
+def startMission(startingLocation):
+    """
+    This function controls the planned mission of drone
+    """
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+#Main body:
+
+startingLocation = LAT_LON_ALT(0.0,0.0,0.0) #startingLocation variable
+waypoint_file = ""	#stores the waypoint file name
+
+#Takes the lat lon and alt value from USER
+while True:
+	try:
+		startingLocation.lat = float(input("Please enter the latitute of starting point:\n"))
+		logger.debug("USER entered latitute value: %s",str(start_lat))
+		if(startingLocation.lat<0 or startingLocation.lat>90):
+			print("Latitude value must be between 0 and 90")
+			continue
+		startingLocation.lon = float(input("Please enter the longitude of starting point:\n"))
+		logger.debug("USER entered longitude value: %s",str(start_lon))
+		if(startingLocation.lon<0 or startingLocation.lon>180):
+			print("Langitude value must be between 0 and 180")
+			continue
+		startingLocation.alt = float(input("Please enter the altitude for the drone:\n"))
+		logger.debug("USER entered altitude value: %s",str(start_alt))
+		if(startingLocation.alt<0):
+			print("Altitude value must be positive")
+			continue
+		break
+	except:
+		logger.error("Oops!  That was no valid lat/lon or altitude.  Try again...")
+
+#Takes the waypoint file name from USER
+while True:
+	waypoint_file = raw_input("Enter the waypoint file name with extension:\n")
+	if os.path.exists(waypoint_file):
+		break
+	else:
+		print("Enter file does not exists. Please re enter correct file")
+		logger.error("Enter file does not exists.")
+		continue
+
+
+#Set up option parsing to get connection string
+import argparse
+parser = argparse.ArgumentParser(description='Demonstrates Seed Plantation Mission in GUIDED mode.')
+parser.add_argument('--connect', help="vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+connection_string = args.connect
+sitl = None
+
+
+#Start SITL if no connection string specified
+if not connection_string:
+	import dronekit_sitl
+	sitl = dronekit_sitl.start_default(lat=start_lat,lon=start_lon)
+	connection_string = sitl.connection_string()
+
+# Connect to the Vehicle
+print('Connecting to vehicle on: %s' % connection_string)
+logger.info('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
+
+#log vechicle attributes:
+print_vechicle_attributes()
+#log vechicle parameters:
+print_vechicle_parameters()
+
+print("Arm and Takeoff")
+logger.info("Arm and Takeoff")
+arm_and_takeoff(startingLocation.alt)
+
+print("Starting mission")
+logger.info("Starting mission")
+
+startMission(startingLocation)
+
+
+print('Return to launch')
+logger.critical("Return to launch")
+while (vehicle.mode.name != "RTL"):
+	vehicle.mode = VehicleMode("RTL")
+	time.sleep(0.1)
+
+#Close vehicle object before exiting script
+print("Close vehicle object")
+logger.info("Close vehicle object")
+vehicle.close()
+
+# Shut down simulator if it was started.
+if sitl is not None:
+	sitl.stop()
+print("Completed...")
+logger.info("Completed...")
